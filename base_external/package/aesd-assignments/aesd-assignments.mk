8f7c19762d9aaff4be4a95c0e677736d46f354ff
jordankooyman <jordankooyman@gmail.com>
2026-02-27 12:29:37 -0700

    Fixed Package Recognition & Install errors

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -4,26 +4,26 @@
 # AESD-ASSIGNMENTS
 #
 ##############################################################
-# File completed with assistance from DeepSeek: https://chat.deepseek.com/share/ke9swxjqsdmp9ceyr9, https://chat.deepseek.com/share/n28hbtrje83qo6o0ek, https://chat.deepseek.com/share/9nslfci45g6xdkw2a2
+# File completed with assistance from DeepSeek: https://chat.deepseek.com/share/ke9swxjqsdmp9ceyr9, https://chat.deepseek.com/share/n28hbtrje83qo6o0ek, https://chat.deepseek.com/share/9nslfci45g6xdkw2a2, https://chat.deepseek.com/share/x5kx4uuj47fj9mkfln, https://chat.deepseek.com/share/j8vw3c9sbf027f7xe7
 
 # Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '2a57738'
+AESD_ASSIGNMENTS_VERSION = 2a57738
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-jordankooyman.git'
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-jordankooyman.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 
-AESD_ASSIGNMENTS_TYPE = kernel-module
+AESD_ASSIGNMENTS_DEPENDENCIES = linux
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	# Build finder-app
 	#$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
 	
 	# Build the AESDChar Kernel Module
-	$(MAKE) -C $(LINUX_DIR) M=$(@D)/aesd-char-driver modules
+	$(MAKE) ARCH=$(KERNEL_ARCH) CROSS_COMPILE=$(TARGET_CROSS) -C $(LINUX_DIR) M=$(@D)/aesd-char-driver modules
 	
 	# Build aesdsocket from server directory with cross-compilation support
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
@@ -45,16 +45,15 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	#$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin
 	
 	# Install the AESDChar kernel module
-	$(MAKE) -C $(LINUX_DIR) M=$(@D)/aesd-char-driver modules_install
-
-	# Update module dependencies
+	$(MAKE) ARCH=$(KERNEL_ARCH) CROSS_COMPILE=$(TARGET_CROSS) INSTALL_MOD_PATH=$(TARGET_DIR) -C $(LINUX_DIR) M=$(@D)/aesd-char-driver modules_install
 	$(HOST_DIR)/sbin/depmod -a -b $(TARGET_DIR) $(LINUX_VERSION_PROBED)
 	
 	# Install aesdsocket executable to /usr/bin
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
 	
 	# Install aesdsocket-start-stop script to /etc/init.d/S99aesdsocket
+	$(INSTALL) -d $(TARGET_DIR)/etc/init.d
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
 
-$(eval $(kernel-module))
+$(eval $(generic-package))
